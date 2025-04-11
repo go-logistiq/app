08737481fc7836a906d341d6c9b64acb2423baa4
Krunoslav Husak <kruno@husak.me>
2025-04-11 22:47:37 +0200

    Add daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,3 @@
 <h1 class="text-3xl font-bold underline">
-  Hello world!
+  LogistiqAPP!
 </h1>
-
-<style lang="postcss">
-  @reference "tailwindcss";
-  :global(html) {
-    background-color: theme(--color-gray-100);
-  }
-</style>
